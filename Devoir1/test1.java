1480dd1a951cafb6f8a4a5f8be0b7fdf49fbaf25
Andre Tran <46461815+gg0andre@users.noreply.github.com>
2019-09-27 00:57:27 -0400

    a

---
+++ Devoir1/test1.java
@@ -0,0 +1,26 @@
+package test;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double x = 2.5;
+		double y = -1.5;
+		int m = 18;
+		int n = 4;
+		String s = "Hello";
+		String t = "World";
+		
+		System.out.println(x+y);
+		System.out.println(x+m);
+		System.out.println(x+n*y-(x+n)*y);
+		System.out.println(5*x-n/n);
+		System.out.println((int)x);
+		System.out.println(Math.round(x));
+		System.out.println(s+t);
+		System.out.println(s+n);
+		System.out.println(s.substring(1, 3));
+		System.out.println(s.length()+t.length());
+	}
+
+}
